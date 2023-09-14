940baa28ee03e28225c11295e5ccab0e18e6d25b
susannaarwen <susannachapman@zoa.io>
2023-09-14 11:46:28 +0100

    add fetch function and render questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import { useState } from 'react'
 import silhouette from './silhouette.png'
 import './App.css'
+import Question from './Question'
+//import apiURL from ????!
 
 function App() {
  
+  const [activeQuestion, setActiveQuestion] = useState(null);
+
+  async function fetchQuestions() {
+    try {
+      const response = await fetch (`${apiURL}/question`);
+      if (!response.ok) {
+        throw new Error(`Fetch failed with status ${response.status}`); 
+      }
+      const questionData = await response.json(); 
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+
 
   return (
     <>
@@ -11,6 +28,11 @@ function App() {
         <h1>The Renewable Energy Investments Quiz</h1>
         <img src={silhouette} id="wind-farm-silhouette" alt="a black silhouette of some drawings of wind turbines in front of a white background" />
       </div>
+      <div id="active-question">
+        {activeQuestion && <Question  />}
+        
+      </div>
+}
       
     </>
   )
